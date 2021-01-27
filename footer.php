24bfad8c77e4ac13a4c6c459125946129d772658
ardikalana <78089341+ardikalana@users.noreply.github.com>
2021-01-27 21:08:08 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> PT.Sei Balai Green Energy |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Data</a>
+            <a href="#">Perusahaan</a>
+        </div>
+    </div>
+</div>
